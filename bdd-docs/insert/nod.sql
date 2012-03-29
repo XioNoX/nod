e711b6e14c94880784bb078cf14081621d6ae2dc
Thomas Belin <ThomasBelin4@gmail.com>
2012-03-29 21:16:42 +0200

    Removing prefixes in front of table fields names Pluralization of the databases tables

--- bdd-docs/insert/nod.sql
+++ bdd-docs/insert/nod.sql
@@ -26,12 +26,12 @@ SET time_zone = "+00:00";
 -- Table structure for table `contact`
 --
 
-CREATE TABLE IF NOT EXISTS `contact` (
-  `contact_id` int(11) NOT NULL AUTO_INCREMENT,
-  `contact_phone` int(11) NOT NULL,
-  `contact_email` int(11) NOT NULL,
-  `contact_website` int(11) NOT NULL,
-  PRIMARY KEY (`contact_id`)
+CREATE TABLE IF NOT EXISTS `contacts` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `phone` int(11) NOT NULL,
+  `email` int(11) NOT NULL,
+  `website` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
@@ -41,11 +41,11 @@ CREATE TABLE IF NOT EXISTS `contact` (
 --
 
 CREATE TABLE IF NOT EXISTS `fees` (
-  `fee_id` int(11) NOT NULL AUTO_INCREMENT,
-  `fee_individual` int(11) NOT NULL,
-  `fee_group` int(11) NOT NULL,
-  `fee_subscription` int(11) NOT NULL,
-  PRIMARY KEY (`fee_id`)
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `individual` int(11) NOT NULL,
+  `group` int(11) NOT NULL,
+  `subscription` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
@@ -55,13 +55,13 @@ CREATE TABLE IF NOT EXISTS `fees` (
 --
 
 CREATE TABLE IF NOT EXISTS `gps` (
-  `gps_id` int(11) NOT NULL AUTO_INCREMENT,
-  `gps_longitude` float NOT NULL,
-  `gps_latitude` float NOT NULL,
-  `gps_address` varchar(255) COLLATE utf8_bin NOT NULL,
-  `gps_zip` int(11) NOT NULL,
-  `gps_city` varchar(255) COLLATE utf8_bin NOT NULL,
-  PRIMARY KEY (`gps_id`)
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `longitude` float NOT NULL,
+  `latitude` float NOT NULL,
+  `address` varchar(255) COLLATE utf8_bin NOT NULL,
+  `zip` int(11) NOT NULL,
+  `city` varchar(255) COLLATE utf8_bin NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
@@ -71,18 +71,18 @@ CREATE TABLE IF NOT EXISTS `gps` (
 --
 
 CREATE TABLE IF NOT EXISTS `parks` (
-  `park_id` int(11) NOT NULL AUTO_INCREMENT,
-  `park_games` tinyint(1) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `games` tinyint(1) NOT NULL,
   `parc_picnic_furniture` tinyint(1) NOT NULL,
-  `park_wading_pool` tinyint(1) NOT NULL,
-  `park_fountain` tinyint(1) NOT NULL,
-  `park_restrooms` tinyint(1) NOT NULL,
-  `park_shed` tinyint(1) NOT NULL,
-  `park_disabled_access` tinyint(1) NOT NULL,
-  `park_vegetal_collection` tinyint(1) NOT NULL,
-  `park_keeper` tinyint(1) NOT NULL,
+  `wading_pool` tinyint(1) NOT NULL,
+  `fountain` tinyint(1) NOT NULL,
+  `restrooms` tinyint(1) NOT NULL,
+  `shed` tinyint(1) NOT NULL,
+  `disabled_access` tinyint(1) NOT NULL,
+  `vegetal_collection` tinyint(1) NOT NULL,
+  `keeper` tinyint(1) NOT NULL,
   `parc_close_garden` tinyint(1) NOT NULL,
-  PRIMARY KEY (`park_id`)
+  PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
@@ -91,17 +91,17 @@ CREATE TABLE IF NOT EXISTS `parks` (
 -- Table structure for table `point_of_interest`
 --
 
-CREATE TABLE IF NOT EXISTS `point_of_interest` (
-  `poi_id` int(11) NOT NULL AUTO_INCREMENT,
-  `poi_label` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
-  `poi_description` text CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
-  `poi_contact_id` int(11) NOT NULL,
-  `poi_gps_id` int(11) NOT NULL,
-  `poi_type` varchar(80) NOT NULL,
-  `poi_type_id_in_table` int(11) NOT NULL,
-  `poi_schedule` int(11) NOT NULL,
-  `poi_schedule_exc` int(11) NOT NULL,
-  PRIMARY KEY (`poi_id`)
+CREATE TABLE IF NOT EXISTS `points_of_interest` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `label` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `description` text CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `contact_id` int(11) NOT NULL,
+  `gps_id` int(11) NOT NULL,
+  `type` varchar(80) NOT NULL,
+  `type_id_in_table` int(11) NOT NULL,
+  `schedule` int(11) NOT NULL,
+  `schedule_exc` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
@@ -110,11 +110,11 @@ CREATE TABLE IF NOT EXISTS `point_of_interest` (
 -- Table structure for table `popularity`
 --
 
-CREATE TABLE IF NOT EXISTS `popularity` (
-  `popularity_id` int(11) NOT NULL AUTO_INCREMENT,
-  `popularity_rate` int(11) NOT NULL,
-  `popularite_id_poi` int(11) NOT NULL,
-  PRIMARY KEY (`popularity_id`)
+CREATE TABLE IF NOT EXISTS `popularities` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `rate` int(11) NOT NULL,
+  `poi_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
@@ -123,16 +123,16 @@ CREATE TABLE IF NOT EXISTS `popularity` (
 -- Table structure for table `schedule`
 --
 
-CREATE TABLE IF NOT EXISTS `schedule` (
-  `schedule_id` int(11) NOT NULL AUTO_INCREMENT,
-  `schedule_label` varchar(45) COLLATE utf8_bin NOT NULL,
-  `schedule_date_start_period` date NOT NULL,
-  `schedule_date_end_period` date NOT NULL,
-  `schedule_time_start` time NOT NULL,
-  `schedule_time_end` time NOT NULL,
-  `schedule_days` varchar(7) COLLATE utf8_bin NOT NULL,
-  `schedule_fee_id` int(11) NOT NULL,
-  PRIMARY KEY (`schedule_id`)
+CREATE TABLE IF NOT EXISTS `schedules` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `label` varchar(45) COLLATE utf8_bin NOT NULL,
+  `date_start_period` date NOT NULL,
+  `date_end_period` date NOT NULL,
+  `time_start` time NOT NULL,
+  `time_end` time NOT NULL,
+  `days` varchar(7) COLLATE utf8_bin NOT NULL,
+  `fee_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
@@ -141,11 +141,11 @@ CREATE TABLE IF NOT EXISTS `schedule` (
 -- Table structure for table `stops_tan_poi`
 --
 
-CREATE TABLE IF NOT EXISTS `stops_tan_poi` (
-  `stops_tan_poi_id` int(11) NOT NULL AUTO_INCREMENT,
-  `stops_poi_id` int(11) NOT NULL,
-  `stops_tan_stop_id` int(11) NOT NULL,
-  PRIMARY KEY (`stops_tan_poi_id`)
+CREATE TABLE IF NOT EXISTS `tan_stop_poi` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `poi_id` int(11) NOT NULL,
+  `tan_stop_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
@@ -155,12 +155,12 @@ CREATE TABLE IF NOT EXISTS `stops_tan_poi` (
 --
 
 CREATE TABLE IF NOT EXISTS `tan_stops` (
-  `stop_id` int(11) NOT NULL AUTO_INCREMENT,
-  `stop_number_of_line` int(11) NOT NULL,
-  `stop_name_stop` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
-  `stop_longitude` float NOT NULL,
-  `stop_latitude` float NOT NULL,
-  PRIMARY KEY (`stop_id`)
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `number_of_line` int(11) NOT NULL,
+  `name_stop` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `longitude` float NOT NULL,
+  `latitude` float NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 
